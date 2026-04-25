5cda4d72375d23ef3716a2eddcae542fe363a0cd
Stephan Kern <stephan@swmmrman.com>
2026-04-25 01:05:16 -0600

    Setup pwm, pwm doesn't have toggle

--- from_scratch/src/main.rs
+++ from_scratch/src/main.rs
@@ -2,8 +2,9 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Level, Output};
+// use embassy_rp::gpio::{Level, Output};
 use embassy_time::Timer;
+use embassy_rp::pwm::{Pwm,Config};
 
 
 #[panic_handler]
@@ -14,7 +15,9 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_6, Level::Low);
+    let mut pwm_config: Config = Default::default();
+    let mut led = Pwm::new_output_a(p.PWM_SLICE3, p.PIN_6, pwm_config);
+    pwm_config.phase_correct = true;
     loop {
         led.toggle();
         Timer::after_millis(500).await;
