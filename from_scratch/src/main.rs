4371b30760e44e19beacc996d9437b20a668bcc7
Stephan Kern <stephan@swmmrman.com>
2026-04-25 00:07:45 -0600

    Done with 5.4

--- from_scratch/src/main.rs
+++ from_scratch/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
+
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-fn main() {}
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {}
