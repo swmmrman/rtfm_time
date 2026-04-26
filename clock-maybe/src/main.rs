2ca7a07695cde60424f7ca36a504caa7d2f23900
Stephan Kern <stephan@swmmrman.com>
2026-04-25 23:09:33 -0600

    more imports/depends

--- clock-maybe/src/main.rs
+++ clock-maybe/src/main.rs
@@ -5,12 +5,13 @@ use embassy_executor::Spawner;
 use embassy_rp as hal;
 use embassy_rp::i2c;
 
+use core::fmt::Write;
+use lcd::*;
+
 //Panic Handler
 use panic_probe as _;
 // Defmt Logging
 use defmt_rtt as _;
-
-use lcd;
 
 fn main() {
     println!("Hello, world!");
