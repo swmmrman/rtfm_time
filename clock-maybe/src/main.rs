816016887e560eb9fd5bbd4e831fe046dccea959
Stephan Kern <stephan@swmmrman.com>
2026-04-25 23:04:33 -0600

    Imports

--- clock-maybe/src/main.rs
+++ clock-maybe/src/main.rs
@@ -1,6 +1,17 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
+use embassy_rp as hal;
+use embassy_rp::i2c;
+
+//Panic Handler
+use panic_probe as _;
+// Defmt Logging
+use defmt_rtt as _;
+
+use lcd;
+
 fn main() {
     println!("Hello, world!");
 }
